e791d98794f266ef427eac40158f140c0b286ddc
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2026-03-21 14:06:52 -1200

    routes fixed

--- x_ray/urls.py
+++ x_ray/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .views import upload_xray, detect_xray
 
-urlpatterns = [path("upload/", upload_xray), path("detect/", detect_xray)]
+urlpatterns = [path("", upload_xray), path("detect/", detect_xray)]
